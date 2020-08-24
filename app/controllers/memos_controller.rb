5dee4ab8b4463a0b812996f04ae5ff667a3b5e9e
AkioShimono <noaokku@gmail.com>
2020-08-24 19:29:51 +0900

    initial commit

---
+++ app/controllers/memos_controller.rb
@@ -0,0 +1,37 @@
+class MemosController < ApplicationController
+    def index
+        #トップページが呼ばれたときに動作するアクション
+        @memos = Memo.all
+        @categories = Category.all
+    end
+
+    def new
+        #新規作成ページが呼ばれた田ときに動作するアクション
+        @categories = Category.all
+
+    end
+
+    def create
+        #新しいメモがフォームにPostされたときに動作するアクション
+        Memo.create(title:params["memos"]["title"],body:params["memos"]["body"],category_id:params["memos"]["category_id"])
+        redirect_to "/"
+    end
+
+    def destroy
+        memo = Memo.find(params["id"])
+        memo.destroy
+        redirect_to "/"
+    end
+
+    def edit
+        @memo = Memo.find(params[:id])
+    end
+
+    def update
+        memo = Memo.find(params["id"])
+        memo.title = params["memos"]["title"]
+        memo.body = params["memos"]["body"]
+        memo.save
+        redirect_to "/"
+    end
+end
